b820b2e961c71e8e60bbe16db2e6b360230b816b
adrianatrevinoe <70771428+adrianatrevinoe@users.noreply.github.com>
2023-04-06 16:48:40 -0600

    advances of tutorial for package

---
+++ packaging_tutorial/src/example_package_adrianatrevinoe/__init__.py
@@ -0,0 +1,9 @@
+[build-system]
+requires = ["hatchling"]
+build-backend = "hatchling.build"
+
+# with setuptools
+
+# [build-system]
+# requires = ["setuptools>=61.0"]
+# build-backend = "setuptools.build_meta"
